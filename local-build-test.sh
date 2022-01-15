d0482f08302168df3c650a09c1596e67354874e2
Peter Burns <petercb@users.noreply.github.com>
2022-01-15 10:29:10 -0500

    Add local build test script

---
+++ local-build-test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+set -u
+
+function cleanup {
+    echo "Stopping builder"
+    docker buildx stop container
+}
+
+trap cleanup EXIT
+
+docker buildx create --driver docker-container --name container --node container0 --use
+
+for dockerfile in ${1:-*}/Dockerfile
+do
+    image="$(dirname ${dockerfile})"
+    pushd "${image}"
+    docker buildx build --builder container --load --platform linux/amd64 -t petercb/${image}:latest .
+    container-structure-test test --config container-structure-test.yaml --image petercb/${image}:latest
+    docker buildx build --builder container --platform linux/arm64/v8 -t petercb/${image}:latest .
+    popd
+done
